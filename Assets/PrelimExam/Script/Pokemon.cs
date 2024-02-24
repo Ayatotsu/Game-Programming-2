1066c1d22437417c108e0bdfe32229cec527de99
Edgardo Merelos Jr <edgardomerelosjr@gmail.com>
2024-02-25 01:21:17 +0800

    Initial commit

---
+++ Assets/PrelimExam/Script/Pokemon.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Pokemon", menuName = "ScriptableObjects/Pokemon")]
+public class Pokemon : ScriptableObject
+{
+    public string pName;
+    public int pLevel;
+    public int pHp;
+    public int atk;
+    public int spatk;
+    public int def;
+    public int spdef;
+    public int speed;
+    public Gender gender;
+    public Types types;
+    public Types types2;
+    public Sprite image;
+    public Sprite bgImage;
+
+    public void Attack() 
+    {
+        Debug.Log($"{name} used Tackle!");
+    }
+}
